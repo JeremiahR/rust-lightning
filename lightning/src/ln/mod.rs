5edc847811d81d8729fef24ba4a77267684d0f46
jeremiah <me@jeremiahrogers.com>
2025-03-04 23:57:23 -0500

    mod for lmp

--- lightning/src/ln/mod.rs
+++ lightning/src/ln/mod.rs
@@ -9,31 +9,33 @@
 
 //! Implementations of various parts of the Lightning protocol are in this module.
 
+#![allow(missing_docs)]
+
 #[cfg(any(test, feature = "_test_utils"))]
 #[macro_use]
 pub mod functional_test_utils;
 
-pub mod onion_payment;
-pub mod channelmanager;
+pub mod chan_utils;
 pub mod channel_keys;
 pub mod channel_state;
+pub mod channelmanager;
+mod features;
 pub mod inbound_payment;
 pub mod msgs;
+pub mod onion_payment;
 pub mod peer_handler;
-pub mod chan_utils;
-mod features;
 pub mod script;
 pub mod types;
 
 // TODO: These modules were moved from lightning-invoice and need to be better integrated into this
 // crate now:
-pub mod invoice_utils;
 pub mod bolt11_payment;
+pub mod invoice_utils;
 
 #[cfg(fuzzing)]
 pub mod peer_channel_encryptor;
-#[cfg(not(fuzzing))]
-pub(crate) mod peer_channel_encryptor;
+// #[cfg(not(fuzzing))]
+pub mod peer_channel_encryptor;
 
 #[cfg(fuzzing)]
 pub mod channel;
@@ -52,12 +54,21 @@ pub use onion_utils::create_payment_onion;
 // without the node parameter being mut. This is incorrect, and thus newer rustcs will complain
 // about an unnecessary mut. Thus, we silence the unused_mut warning in two test modules below.
 
-#[cfg(test)]
-#[allow(unused_mut)]
-mod blinded_payment_tests;
 #[cfg(all(test, async_payments))]
 #[allow(unused_mut)]
 mod async_payments_tests;
+#[cfg(test)]
+#[allow(unused_mut)]
+mod async_signer_tests;
+#[cfg(test)]
+#[allow(unused_mut)]
+mod blinded_payment_tests;
+#[cfg(test)]
+#[allow(unused_mut)]
+mod chanmon_update_fail_tests;
+#[cfg(test)]
+#[allow(unused_mut)]
+mod dual_funding_tests;
 #[cfg(test)]
 #[allow(unused_mut)]
 mod functional_tests;
@@ -66,36 +77,27 @@ mod functional_tests;
 mod max_payment_path_len_tests;
 #[cfg(test)]
 #[allow(unused_mut)]
+mod monitor_tests;
+#[cfg(test)]
+#[allow(unused_mut)]
+mod offers_tests;
+#[cfg(test)]
+#[allow(unused_mut)]
+mod onion_route_tests;
+#[cfg(test)]
+#[allow(unused_mut)]
 mod payment_tests;
 #[cfg(test)]
 #[allow(unused_mut)]
 mod priv_short_conf_tests;
 #[cfg(test)]
 #[allow(unused_mut)]
-mod chanmon_update_fail_tests;
+mod reload_tests;
 #[cfg(test)]
 #[allow(unused_mut)]
 mod reorg_tests;
 #[cfg(test)]
 #[allow(unused_mut)]
-mod reload_tests;
-#[cfg(test)]
-#[allow(unused_mut)]
-mod onion_route_tests;
-#[cfg(test)]
-#[allow(unused_mut)]
-mod monitor_tests;
-#[cfg(test)]
-#[allow(unused_mut)]
 mod shutdown_tests;
-#[cfg(test)]
-#[allow(unused_mut)]
-mod async_signer_tests;
-#[cfg(test)]
-#[allow(unused_mut)]
-mod offers_tests;
-#[cfg(test)]
-#[allow(unused_mut)]
-mod dual_funding_tests;
 
 pub use self::peer_channel_encryptor::LN_MAX_MSG_LEN;
